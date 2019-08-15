50749138e2259b53ab2eee13970f43333b17294b
(nus)weikiat <weikiat.leong87@gmail.com>
2019-08-15 11:24:47 +0800

    Updates

---
+++ precache-manifest.43ce0c142d07a08bfa8c650bc80d10f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654b649d867022ab9701c21381745386",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9a0ba281bc4ef8db3c9d",
+    "url": "/Portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9a0ba281bc4ef8db3c9d",
+    "url": "/Portfolio/static/js/main.5df52881.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
