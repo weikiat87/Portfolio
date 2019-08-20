6e1671acfbae4712a3eebb8874ae564433d30624
(nus)weikiat <weikiat.leong87@gmail.com>
2019-08-20 17:32:42 +0800

    fixed intro bg-image (was not scaling correctly to how i wanted.

--- src/Components/IntroComponent.js
+++ src/Components/IntroComponent.js
@@ -13,13 +13,15 @@ const useStyle = makeStyles(theme => ({
     color: theme.palette.primary.contrastText,
     textShadow: "2px 2px 5px #aaaaaa"
   },
-  img: {
-    display:"block",
-    boxShadow: "2px 10px 5px #aaaaaa"
-  },
   wrapper:{
     position:"relative",
-    display:"block"
+    display:"block",
+    backgroundImage:"url(/images/intro-bg.jpg)",
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed",
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+    height: "100vh"
   }
 }));
 
@@ -29,12 +31,6 @@ function IntroComponent() {
     <>
       <Slide direction="down" in={true}>
         <Box className={styles.wrapper}>
-          <img
-            alt=""
-            src="/images/intro-bg.jpg"
-            width="100%"
-            className={styles.img}
-          />
           <Box className={styles.text}>
             <Typography variant="h1">
               I am a
